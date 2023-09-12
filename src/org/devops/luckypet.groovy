07f6470841c3ac60d84acae7400f0c2b3f60e063
linxin <linxin@infras.tech>
2023-09-12 12:43:49 +0800

    d

--- src/org/devops/luckypet.groovy
+++ src/org/devops/luckypet.groovy
@@ -6,15 +6,16 @@ def BuildImageAndPush(option, env, imageAddr, serviceName, tag){
 
     if (env == "dev"){
         sh """
-        docker build -t ${imageAddr}/${serviceName}:${tag} -f Dockerfile_dev .
-        docker push ${imageAddr}/${serviceName}:${tag}
-        docker rmi ${imageAddr}/${serviceName}:${tag}
+        Branch=`echo \$Tag | sed 's/\\//_/g'`
+        docker build -t ${imageAddr}/${serviceName}:\$Branch -f Dockerfile_dev .
+        docker push ${imageAddr}/${serviceName}:\$Branch
+        docker rmi ${imageAddr}/${serviceName}:\$Branch
         """
     }else{
         sh """
-        docker build -t ${imageAddr}/${serviceName}:${tag} -f Dockerfile .
-        docker push ${imageAddr}/${serviceName}:${tag}
-        docker rmi ${imageAddr}/${serviceName}:${tag}
+        docker build -t ${imageAddr}/${serviceName}:\$Branch -f Dockerfile .
+        docker push ${imageAddr}/${serviceName}:\$Branch
+        docker rmi ${imageAddr}/${serviceName}:\$Branch
         """
     }
 }
@@ -24,7 +25,8 @@ def BuildImageAndPush(option, env, imageAddr, serviceName, tag){
 def Publish(option, env, imageAddr, servicename, projectname, tag, servicepath, hostname, jobname, arn) {
     command = """
         cd /home/RD.Center/eks/genDeploy && git pull
-        /usr/local/go/bin/go run /home/RD.Center/eks/genDeploy/genDeploy.go aws-ecr-key ${imageAddr} ${env} ${tag} 1 ${projectname} ${servicename} /home/RD.Center/jenkins/${jobname}
+        Branch=`echo \$Tag | sed 's/\\//_/g'`
+        /usr/local/go/bin/go run /home/RD.Center/eks/genDeploy/genDeploy.go aws-ecr-key ${imageAddr} ${env} \$Branch 1 ${projectname} ${servicename} /home/RD.Center/jenkins/${jobname}
         kubectl -n ${projectname}-${env} delete deployment `kubectl get deployment -n ${projectname}-${env} |grep ${servicename}-deployment|awk '{print \$1}'`
         cd /home/RD.Center/jenkins/${jobname}
         kubectl apply -f deployment.yaml
