f312432f579dc1cd07ecba754c30c18565c549ce
linxin <linxin@infras.tech>
2023-09-12 12:04:54 +0800

    d

--- src/org/devops/luckypet.groovy
+++ src/org/devops/luckypet.groovy
@@ -6,15 +6,15 @@ def BuildImageAndPush(option, env, imageAddr, serviceName, tag){
 
     if (env == "dev"){
         sh """
-        docker build -t ${imageAddr}/${serviceName}:`echo \${tag} | sed 's/\\//_/g'` -f Dockerfile_dev .
-        docker push ${imageAddr}/${serviceName}:`echo \${tag} | sed 's/\\//_/g'`
-        docker rmi ${imageAddr}/${serviceName}:`echo \${tag} | sed 's/\\//_/g'`
+        docker build -t ${imageAddr}/${serviceName}:${tag} -f Dockerfile_dev .
+        docker push ${imageAddr}/${serviceName}:${tag}
+        docker rmi ${imageAddr}/${serviceName}:${tag}
         """
     }else{
         sh """
-        docker build -t ${imageAddr}/${serviceName}:`echo \${tag} | sed 's/\\//_/g'` -f Dockerfile .
-        docker push ${imageAddr}/${serviceName}:`echo \${tag} | sed 's/\\//_/g'`
-        docker rmi ${imageAddr}/${serviceName}:`echo \${tag} | sed 's/\\//_/g'`
+        docker build -t ${imageAddr}/${serviceName}:${tag} -f Dockerfile .
+        docker push ${imageAddr}/${serviceName}:${tag}
+        docker rmi ${imageAddr}/${serviceName}:${tag}
         """
     }
 }
@@ -24,7 +24,7 @@ def BuildImageAndPush(option, env, imageAddr, serviceName, tag){
 def Publish(option, env, imageAddr, servicename, projectname, tag, servicepath, hostname, jobname, arn) {
     command = """
         cd /home/RD.Center/eks/genDeploy && git pull
-        /usr/local/go/bin/go run /home/RD.Center/eks/genDeploy/genDeploy.go aws-ecr-key ${imageAddr} ${env} `echo \${tag} | sed 's/\\//_/g'` 1 ${projectname} ${servicename} /home/RD.Center/jenkins/${jobname}
+        /usr/local/go/bin/go run /home/RD.Center/eks/genDeploy/genDeploy.go aws-ecr-key ${imageAddr} ${env} ${tag} 1 ${projectname} ${servicename} /home/RD.Center/jenkins/${jobname}
         kubectl -n ${projectname}-${env} delete deployment `kubectl get deployment -n ${projectname}-${env} |grep ${servicename}-deployment|awk '{print \$1}'`
         cd /home/RD.Center/jenkins/${jobname}
         kubectl apply -f deployment.yaml
