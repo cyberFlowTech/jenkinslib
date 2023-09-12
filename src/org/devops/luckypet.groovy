9734cb97eaea8da3166fd969a15dbe4407374db6
linxin <linxin@infras.tech>
2023-09-12 11:45:45 +0800

    luckeypet

---
+++ src/org/devops/luckypet.groovy
@@ -0,0 +1,41 @@
+package org.devops
+
+// 024905375334.dkr.ecr.ap-southeast-1.amazonaws.com/infras
+
+def BuildImageAndPush(option, env, imageAddr, serviceName, tag){
+
+    if (env == "dev"){
+        sh """
+        tag=`echo \${tag} | sed 's/\\//_/g'`
+        docker build -t ${imageAddr}/${serviceName}:${tag} -f Dockerfile_dev
+        docker push ${imageAddr}/${serviceName}:${tag}
+        docker rmi ${imageAddr}/${serviceName}:${tag}
+        """
+    }else{
+        sh """
+        tag=`echo \${tag} | sed 's/\\//_/g'`
+        docker build -t ${imageAddr}/${serviceName}:${tag} -f Dockerfile
+        docker push ${imageAddr}/${serviceName}:${tag}
+        docker rmi ${imageAddr}/${serviceName}:${tag}
+        """
+    }
+}
+
+
+
+// 生产作废
+def Publish(option, env, imageAddr, servicename, projectname, tag, servicepath, hostname, jobname, arn) {
+    command = """
+        tag=`echo \${tag} | sed 's/\\//_/g'`
+        cd /home/RD.Center/eks/genDeploy && git pull
+        /usr/local/go/bin/go run /home/RD.Center/eks/genDeploy/genDeploy.go aws-ecr-key ${imageAddr} ${env} ${tag} 1 ${projectname} ${servicename} /home/RD.Center/jenkins/${jobname}
+        kubectl -n ${projectname}-${env} delete deployment `kubectl get deployment -n ${projectname}-${env} |grep ${servicename}-deployment|awk '{print \$1}'`
+        cd /home/RD.Center/jenkins/${jobname}
+        kubectl apply -f deployment.yaml
+        kubectl apply -f service.yaml
+    """
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'AwsEksJumpServer', transfers: [sshTransfer(cleanRemote: false, excludes: '', 
+    execCommand: command, execTimeout: 22000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/jenkins/${env.JOB_NAME}', remoteDirectorySDF: false, removePrefix: '', 
+    sourceFiles: '')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: true)])
+
+}
