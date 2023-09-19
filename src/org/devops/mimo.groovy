49ad163f80cd0699d4490dffd20ae686a47fc922
linxin <linxin@infras.tech>
2023-09-19 18:20:18 +0800

    pymimo

--- src/org/devops/mimo.groovy
+++ src/org/devops/mimo.groovy
@@ -343,8 +343,8 @@ def PublishPyMimo(option, env, imageAddr, servicename, projectname, tag, service
         /usr/local/go/bin/go run /home/RD.Center/eks/genDeploy/genDeploy.go aws-ecr-key ${imageAddr} ${env} ${tag} 1 ${projectname} ${servicename} /home/RD.Center/jenkins/${jobname}
         kubectl -n ${projectname}-${env} delete deployment `kubectl get deployment -n ${projectname}-${env} |grep ${servicename}-deployment|awk '{print \$1}'`
         cd /home/RD.Center/jenkins/${jobname}
-        #kubectl apply -f deployment.yaml
-        #kubectl apply -f service.yaml
+        kubectl apply -f deployment.yaml
+        kubectl apply -f service.yaml
     """
     sshPublisher(publishers: [sshPublisherDesc(configName: 'AwsEksJumpServer', transfers: [sshTransfer(cleanRemote: false, excludes: '', 
     execCommand: command, execTimeout: 22000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/jenkins/${env.JOB_NAME}', remoteDirectorySDF: false, removePrefix: '', 
