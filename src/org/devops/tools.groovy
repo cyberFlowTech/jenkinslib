ee0c11d82cf8fafc6faacd0851186c9733383d6d
linx01 <18810463426@163.com>
2023-10-27 10:09:52 +0800

    dev

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -143,7 +143,7 @@ def NotifyV2(envi,result) {
     }
 
     sh """
-curl -H "Content-Type: application/json" -H "type: info" -X POST -d \$'{\"api\":\"m_1691395720\",\"data\":\"### ${text} ###\"}' "https://web3.mimo.immo/notify/notify"
+curl -H "Content-Type: application/json" -H "type: info" -X POST -d '{\"api\":\"m_1691395720\",\"data\":\"### ${text} ###\"}' "https://web3.mimo.immo/notify/notify"
     """
 
 
