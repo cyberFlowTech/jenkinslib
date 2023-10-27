1250c791b624a968c3b53ef8ffd35ea34d62915e
linx01 <18810463426@163.com>
2023-10-27 19:53:42 +0800

    d

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -143,7 +143,7 @@ def NotifyV2(envi,result) {
     }
 
     sh """
-    curl -X POST -H Content-Type:application/json -H type:info -d \'{"api":"m_1691395720","data":"${text}\\n申请人:${env.BUILD_USER}\\n构建名称:${env.JOB_NAME}"}\' https://web3.mimo.immo/notify/notify'
+    curl -X POST -H Content-Type:application/json -H type:info -d \'{"api":"m_1691395720","data":"${text}\\n申请人:${env.BUILD_USER}\\n构建名称:${env.JOB_NAME}"}\' https://web3.mimo.immo/notify/notify
     """
 
 }
